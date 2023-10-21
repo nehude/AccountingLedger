836cf29187e26e2b788dd3a83749559b7f16b817
Neha Udeshi <nehaudeshi@Nehas-Air.attlocal.net>
2023-10-21 14:29:30 -0500

    initial commit

---
+++ AccountingLedger/src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isRunning = true;
+
+        while (isRunning) {
+            displayMenu();
+            String choice = scanner.nextLine().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    //addDeposit(scanner);
+                    break;
+                case "P":
+                    //makePayment(scanner);
+                    break;
+                case "L":
+                    //displayLedger(scanner);
+                    break;
+                case "X":
+                    System.out.println("Thank you. Goodbye :)");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Not a valid input. Please try again.");
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("Welcome to the Accounting Home Screen");
+        System.out.println("What would you like to do today?");
+        System.out.println("D) Add Deposit");
+        System.out.println("P) Make Payment (Debit)");
+        System.out.println("L) Ledger");
+        System.out.println("X) Exit");
+        System.out.print("Please enter D, P, L, or X: ");
+    }
+}
